b5f74030a67acc03d00250eee0bc166d7e0de3a9
BrendanLoBuglio <B.LoBuglio@gmail.com>
2017-01-22 13:48:22 -0800

    Title screen for client screen

---
+++ GGJ Cacophony/Assets/Scripts/WikiPlayerManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WikiPlayerManager : MonoBehaviour
+{
+    private InputField mInputField;
+    private EntryField mEntryField;
+    [SerializeField] private MorseDisplayConsole mMorseDisplayConsole;
+    [SerializeField] private TextLog mTextLog;
+
+    public void Start()
+    {
+        mInputField = GetComponent<InputField>();
+        mEntryField = GetComponent<EntryField>();
+    }
+
+    public void OnEndTextEdit(string message)
+    {
+        if (Input.GetKey(KeyCode.Return) && !string.IsNullOrEmpty(mInputField.text)) {
+            mEntryField.SendTextMessage();
+            mEntryField.Reset();
+            mTextLog.AddTextLine(message);
+            mMorseDisplayConsole.Show();
+        }
+    }
+}
